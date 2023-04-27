5cd1321b2a521998721179e7f459c43cdb93f4d0
dessoky <mmdesso@gmail.com>
2023-04-27 15:54:12 +0200

    first

---
+++ pollsapi/polls/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
+
+urlpatterns = [
+    path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/",CreateVote.as_view(), name="create_view"),
+]
+
+
+
+# urlpatterns = [
+#     path("polls/", PollList.as_view(), name="polls_list"),
+#     path("polls/<int:pk>", PollDetail.as_view(), name="polls_detail"),
+#     path("choices/", ChoiceList.as_view(), name="choice_list"),
+#     path("vote/", CreateVote.as_view(), name="create_vote"),
+# ]
+
