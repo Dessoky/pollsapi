b784d4f6773d514ef7fc507b21c3c9a51b6d0bb9
dessoky <mmdesso@gmail.com>
2023-04-30 11:08:43 +0300

    finish

--- pollsapi/polls/apiviews.py
+++ pollsapi/polls/apiviews.py
@@ -1,21 +1,23 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets, status
 from rest_framework.views import APIView
-from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+from django.contrib.auth import authenticate
 from django.shortcuts import get_object_or_404
 
 from .models import Poll, Choice
-from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer, UserSerializer
 
 
-class PollList(generics.ListCreateAPIView):
+class PollViewSet(viewsets.ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
-
-class PollDetail(generics.RetrieveDestroyAPIView):
-    queryset = Poll.objects.all()
-    serializer_class = PollSerializer
+    def destroy(self, request, *args, **kwargs):
+        poll = Poll.objects.get(pk=self.kwargs["pk"])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not delete this poll.")
+        return super().destroy(request, *args, **kwargs)
 
 
 class ChoiceList(generics.ListCreateAPIView):
@@ -23,6 +25,12 @@ class ChoiceList(generics.ListCreateAPIView):
         queryset = Choice.objects.filter(poll_id=self.kwargs["pk"])
         return queryset
     serializer_class = ChoiceSerializer
+
+    def post(self, request, *args, **kwargs):
+        poll = Poll.objects.get(pk=self.kwargs["pk"])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not create choice for this poll.")
+        return super().post(request, *args, **kwargs)
 
 
 class CreateVote(APIView):
@@ -39,7 +47,23 @@ class CreateVote(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class UserCreate(generics.CreateAPIView):
+    authentication_classes = ()
+    permission_classes = ()
+    serializer_class = UserSerializer
 
+
+class LoginView(APIView):
+    permission_classes = ()
+
+    def post(self, request,):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token" : user.auth_token.key})
+        else:
+            return Response({"error" : "Wrong Credentials"}, status = status.HTTP_400_BAD_REQUEST)
 
 
 # class PollList(APIView):
@@ -54,6 +78,3 @@ class CreateVote(APIView):
 #         poll = get_object_or_404(Poll, pk=pk)
 #         data = PollSerializer(poll).data
 #         return Response(data)
-
-
-
--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
+from rest_framework.routers import DefaultRouter
+from .apiviews import ChoiceList, CreateVote, PollViewSet, UserCreate, LoginView
+
+router = DefaultRouter()
+router.register('polls', PollViewSet, basename='polls')
 
 urlpatterns = [
     path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/",CreateVote.as_view(), name="create_view"),
+    path("users/", UserCreate.as_view(), name="user_create"),
+    path("login/", LoginView.as_view(), name = "login"),
 ]
 
+urlpatterns += router.urls
 
 
 # urlpatterns = [
